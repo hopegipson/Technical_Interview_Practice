adf331477511e00ab90ce8f299ecd2a0f8123c71
Hope Gipson <gipson.hope@gmail.com>
2021-05-11 15:06:07 -0600

    Create question.js

---
+++ question.js
@@ -0,0 +1,43 @@
+
+
+
+// https://jsonmock.hackerrank.com/api/movies
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+
+// fetch('http://example.com/movies.json')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+// HOC 
+
+// reactivity of React
+// hooks - useCallback, useMemo
+
+function movieList() {	
+	const [movies, setMovies] = useState([])
+
+	useEffect(()=> {
+		fetch('https://jsonmock.hackerrank.com/api/movies')
+			.then(response => response.json())
+			.then(res => setState(res.data))
+		}
+	}, [])
+
+	return (
+		<div>
+
+		<ul>
+		{movies.map(movie => (
+			<li key={movie.imdbID}>
+			<h2> {movie.title} </h2>
+			<h3> {movie.year} </h3>
+			</li>
+		))}
+		</ul>
+	
+		</div>
+	)
+
+}
